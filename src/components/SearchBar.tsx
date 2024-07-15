52da1a14919222da24fb9e577bc74c50032fe9db
Rameez <shaikhrameez516@gmail.com>
2024-07-15 14:43:15 +0530

    again same errors

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,25 +5,17 @@ import { Search } from 'lucide-react';
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
-  const handleInputChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    console.log('Search:', searchQuery);
-  };
+  
 
   return (
-    <form onSubmit={handleSubmit} className="relative max-w-screen-xl mx-auto ">
+    <form  className="relative max-w-screen-xl mx-auto ">
       <div className="h-[50px] flex items-center border border-gray-400 rounded-lg p-2 bg-white">
         
         <input
           type="text"
           placeholder="Search "
           value={searchQuery}
-          onChange={handleInputChange}
+        
           className="w-full outline-none text-base text-gray-700 focus:outline-none placeholder:text-gray-400"
         />
         <button
